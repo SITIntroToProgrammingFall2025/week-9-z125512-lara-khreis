a4017cdc5354a9faf5e28d8cdb3ba74f666414eb
z125512-lara-khreis <z125512@shibaura-it.ac.jp>
2025-12-09 20:14:16 +0900

    Upload of file ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int a[5];
+
+    printf("Enter 5 numbers:\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    // bubble sort
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (a[j] > a[j+1])
+            {
+                int tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
+            }
+        }
+    }
+    printf("sorted array:\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
